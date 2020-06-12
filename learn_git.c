530040359a8803078e35d5ee922137b2d5294ef3
Boreas-99 <boreas101@protonmail.com>
2020-06-12 14:13:41 +0530

    test 3

--- learn_git.c
+++ learn_git.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "test_1.c"
 #include "test_2.c"
+#include "test_3.c"
 
 void main(){
 	printf("Test 1\n");
 	func_1();
 	func_2();
+	func_3();
 }
---
+++ test_3.c
@@ -0,0 +1,4 @@
+void func_3()
+{
+	printf("What  a test 3\n");
+}
